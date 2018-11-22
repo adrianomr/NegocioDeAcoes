2a133ba5de420bb4843c767031257b46f0d2ca2a
Adriano Rodrigues <adrianomrodrigues93@gmail.com>
2018-11-22 19:33:43 -0200

    Initial commit (Projeto construído via SPRING INITIALIZR)

---
+++ src/main/java/NegocioDeAcoes/NegocioDeAcoes/NegocioDeAcoesApplication.java
@@ -0,0 +1,12 @@
+package NegocioDeAcoes.NegocioDeAcoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocioDeAcoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegocioDeAcoesApplication.class, args);
+	}
+}
